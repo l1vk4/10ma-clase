fcf28fb0400759018f1cb2b45782dff764b3678b
l1vk4 <livka.kutz@gmail.com>
2025-10-23 15:08:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const portfolio = document.querySelector("#porotito");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+        let trabajos = await consulta.json();
+        console.log(trabajos);
+        trabajos.forEach((v) => {
+            portfolio.innerHTML += `<div class="col">
+                        <div class="card shadow-sm h-100">
+                            <img src="${v.photo}" class="card-img-top" alt="${v.title}">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">${v.title}</h5>
+                                <p class=" flex-grow-1">${v.description}</p>
+                                <div class="d-flex justify-content-between align-items-center mt-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">${v.category}</button>
+                                    </div>
+                                    <span class="date-badge">${v.date}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:", error);
+    }
+}
+
+datos("https://raw.githubusercontent.com/l1vk4/10ma-clase/refs/heads/main/datos.json");
